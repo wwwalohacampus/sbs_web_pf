8736d2dcd5e929fee0c74caae81702303d9d1c0e
wwwalohacampus <h850415@naver.com>
2022-01-18 17:48:03 +0900

    1

---
+++ 포르미/js/script.js
@@ -0,0 +1,24 @@
+$(function() {
+
+    $('.slide-container').slick({
+        dots: true,
+        arrows: true,
+
+        autoplay: true,                 // 자동재생 여부
+        autoplaySpeed: 3000,            // 자동재생 슬라이드 시간
+        infinite: true,                 // 무한 반복 
+        speed:500,                      // 슬라이드가 전환되는 시간
+
+
+        // centerMode: true,            // 센터 모드
+        // centerPadding: '60px',       // 센터 모드 시, 내부여백
+        slidesToShow: 1,                // 보여질 슬라이드 개수
+        slidesToScroll: 1,              // 스크롤될 슬라이드 개수
+
+
+        pauseOnDotsHover: true,         // 페이지네이션 호버 시 자동재생 멈춤
+        pauseOnFocus: true,             // 포커스 시, 자동재생 멈춤
+        pauseOnHover: true,             // 호버 시, 자동재생 멈춤
+    })
+
+})
